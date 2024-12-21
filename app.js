ae40ec6559b823ed8acce148b1cc24dc72485e71
Danieldaguy <160911858+Danieldaguy@users.noreply.github.com>
2024-12-21 15:01:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, push, onValue, update, remove, get } from "https://ww
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAw6IN7sKZ3q8kcC0yuLB6cNhxLJ0QHUQU",
-  authDomain: "ember-studios-chatroom-73885.firebaseapp.com",
-  databaseURL: "https://ember-studios-chatroom-73885-default-rtdb.firebaseio.com",
-  projectId: "ember-studios-chatroom-73885",
-  storageBucket: "ember-studios-chatroom-73885.appspot.com",
-  messagingSenderId: "563168350862",
-  appId: "1:563168350862:web:c801f8696f7ab9f5e8efdf",
-  measurementId: "G-LQDGZVJ2JM"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
