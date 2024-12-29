c6e208cee886f2e302aa9d67725d0a5a96736ea3
Danieldaguy <160911858+Danieldaguy@users.noreply.github.com>
2024-12-29 15:31:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getDatabase, ref, push, onValue, update, remove, get } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
 
@@ -25,14 +22,14 @@ const suspiciousActivity = new Map();
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAw6IN7sKZ3q8kcC0yuLB6cNhxLJ0QHUQU",
-  authDomain: "ember-studios-chatroom-73885.firebaseapp.com",
-  databaseURL: "https://ember-studios-chatroom-73885-default-rtdb.firebaseio.com",
-  projectId: "ember-studios-chatroom-73885",
-  storageBucket: "ember-studios-chatroom-73885.appspot.com",
-  messagingSenderId: "563168350862",
-  appId: "1:563168350862:web:c801f8696f7ab9f5e8efdf",
-  measurementId: "G-LQDGZVJ2JM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase with enhanced security
