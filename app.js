cb3658c82e9f38579f0e6d931e2f5edcb6237405
Danieldaguy <160911858+Danieldaguy@users.noreply.github.com>
2025-01-13 20:40:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_SUPABASE_KEY";
+const supabaseUrl = "https://jpphrvektvbpdxuvtgmw.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwcGhydmVrdHZicGR4dXZ0Z213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1MDE0ODUsImV4cCI6MjA1MTA3NzQ4NX0.3gyADNnD_r9ERElETL8eg5OQVn9wQ3o3RMAC3JkNn9Q";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const messagesDiv = document.getElementById("messages");
